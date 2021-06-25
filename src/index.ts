1fc962c4d5d7ac0c39af6e42a11abbe390dabe3a
Thomas Rainford <thomas@rainfords.net>
2021-06-25 17:17:44 +1200

    changes

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ const main = async () => {
          }),
          cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-            httpOnly: true,
+            //httpOnly: true,
             sameSite: "lax", // csrf
             secure: __prod__, // cookie only works in https
             domain: __prod__ ? ".herokuapp.com" : undefined
