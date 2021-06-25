421024556ae0537861887b12e6f40ec35478fb47
Thomas Rainford <thomas@rainfords.net>
2021-06-26 10:39:28 +1200

    Secure cookie.

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ const main = async () => {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
             httpOnly: true,
             sameSite: "lax", // csrf
-            secure: !__prod__, // cookie only works in https
+            secure: __prod__, // cookie only works in https
             domain: !__prod__ ? ".herokuapp.com" : undefined
          },
          saveUninitialized: false,
