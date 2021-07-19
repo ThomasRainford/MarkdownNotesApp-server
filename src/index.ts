029072e484f5387edb851b78ff16b894c0b6b665
Thomas Rainford <thomas@rainfords.net>
2021-07-19 12:39:44 +1200

    changes

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const main = async () => {
    const orm = await MikroORM.init(ormConfig)
 
    const app = express()
-   app.set('trust proxy', 1)
+   //app.set('trust proxy', 1)
    app.use(
       cors({
          origin: process.env.CORS_ORIGIN,
