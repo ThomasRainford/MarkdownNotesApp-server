f115c894c553c3b4107123d3ccb20140526cf896
Thomas Rainford <thomas@rainfords.net>
2022-02-05 15:57:28 +1300

    Create collections for each user.

--- src/__tests__/utils/seeder.ts
+++ src/__tests__/utils/seeder.ts
@@ -1,15 +1,15 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import argon2 from "argon2";
+import { Collection } from "../../entities/Collection";
 import { User } from "../../entities/User";
 
 export const seed = async (em: EntityManager<IDatabaseDriver<Connection>>) => {
-  const users = await createUsers();
+  const users = await createUsers(em);
 
-  const collections = createCollections(em, users);
-  console.log(collections);
+  createCollections(em, users);
 };
 
-const createUsers = async () => {
+const createUsers = async (em: EntityManager<IDatabaseDriver<Connection>>) => {
   const users: User[] = [];
   let hashedPassword = await argon2.hash("password1");
   const user1 = new User({
@@ -35,13 +35,40 @@ const createUsers = async () => {
     username: "User4",
     password: hashedPassword,
   });
+  await em.populate(user1, ["collections"]);
+  await em.populate(user2, ["collections"]);
+  await em.populate(user1, ["collections"]);
+
+  await em.persistAndFlush([user1, user2, user3]);
   users.push(user1, user2, user3, user4);
   return users;
 };
 
-const createCollections = (
+const createCollections = async (
   em: EntityManager<IDatabaseDriver<Connection>>,
   users: User[]
 ) => {
-  console.log(em, users);
+  for (const user of users) {
+    const collection1 = new Collection({
+      title: "Collection 1",
+      visibility: "public",
+    });
+    const collection2 = new Collection({
+      title: "Collection 2",
+      visibility: "public",
+    });
+    const collection3 = new Collection({
+      title: "Collection 3",
+      visibility: "private",
+    });
+    collection1.owner = user;
+    collection2.owner = user;
+    collection3.owner = user;
+    user.collections.add(collection1, collection2, collection3);
+    await em.populate(collection1, ["owner", "lists"]);
+    await em.populate(collection2, ["owner", "lists"]);
+    await em.populate(collection3, ["owner", "lists"]);
+
+    await em.persistAndFlush([collection1, collection2, collection3]);
+  }
 };
