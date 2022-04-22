827b4fb3a40e3291ef2766a4eaea48826186ebf4
Thomas Rainford <thomas@rainfords.net>
2022-04-23 10:56:08 +1200

    Add boilerplate for remaining noteslist tests.

---
+++ src/__tests__/notesList/updateNotes.test.ts
@@ -0,0 +1,40 @@
+import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import Application from "../../application";
+import mikroOrmConfig from "../utils/mikro-orm.config";
+import { seed } from "../utils/seeder";
+import { dropDb } from "../utils/utils";
+
+let application: Application;
+let em: EntityManager<IDatabaseDriver<Connection>>;
+
+describe("UpdateNote Mutation", () => {
+  beforeAll(async () => {
+    application = new Application();
+
+    await application.connect(mikroOrmConfig);
+    await application.initTest();
+
+    em = application.orm.em.fork();
+
+    await seed(application.orm.em);
+  });
+
+  afterAll(async () => {
+    try {
+      await dropDb();
+
+      application.expressServer.close();
+      await application.orm.close();
+      await application.apolloServer.stop();
+    } catch (error: any) {
+      console.log(error);
+    }
+  });
+
+  afterEach(async () => {
+    await dropDb();
+    await seed(application.orm.em);
+  });
+
+  it("should update a note successfully.", async () => {});
+});
