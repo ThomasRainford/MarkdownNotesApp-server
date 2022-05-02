cc0976b41e919f6c20d2a8d0a9c38a0e655c324e
Thomas Rainford <thomas@rainfords.net>
2022-05-02 17:29:45 +1200

    Add test suite for 'updateCollection' resolver.

--- src/utils/validateVisibility.ts
+++ src/utils/validateVisibility.ts
@@ -1,14 +1,13 @@
-import { Error } from '../resolvers/object-types/Error'
+import { Error } from "../resolvers/object-types/Error";
 
 export const validateVisibility = (visibility: string): Error | null => {
+  const validVisibilities = ["public", "private"];
+  if (!validVisibilities.includes(visibility)) {
+    return {
+      property: "visibility",
+      message: "Visibility can only be public or private.",
+    };
+  }
 
-   if (visibility !== 'public' && visibility !== 'private') {
-      return {
-         property: 'visibility',
-         message: 'Visibility can only be public or private.'
-      }
-   }
-
-   return null
-
-}
+  return null;
+};
